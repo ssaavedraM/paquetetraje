c777ff67781e410ed05d043dec87fd6bde69ff21
Sergio Saavedra <32399130+ssaavedraM@users.noreply.github.com>
2023-08-13 18:04:56 -0400

    Commit Inicial

---
+++ src/main/java/ssaavedra/cl/paquetetraje/PaquetetrajeApplication.java
@@ -0,0 +1,13 @@
+package ssaavedra.cl.paquetetraje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaquetetrajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaquetetrajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssaavedra/cl/paquetetraje/PaquetetrajeApplicationTests.java
@@ -0,0 +1,13 @@
+package ssaavedra.cl.paquetetraje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaquetetrajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
